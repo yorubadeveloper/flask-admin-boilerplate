be47d3761eaaf61d91df93cb6657416cc64b62bf
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-05-31 03:03:57 +0100

    The login div is not in the center and it redirects the user after login

--- static/js/checkLoginpassword.js
+++ static/js/checkLoginpassword.js
@@ -1,38 +1,42 @@
-$('#login').submit(function() {
-    $('#button').prop('disabled', true);
-    $('#img').show();
-            event.preventDefault()
-            $.ajax({
-                url: '/checkloginpassword',
-                data: $(this).serialize(),
-                type: 'POST',
-                success: function(response) {
-                    $('#img').hide();
-                    if(response === "correct"){
-                        swal.fire(
-                            {
-                                icon: 'success',
-                                title: 'Login Successful',
-                              }
-                        ).then((result) => {
-                            window.location.href = "/";
-                            });
-                    }
-                    else if(response === "wrong"){
-                        swal.fire(
-                            {
-                                icon: 'error',
-                                title: 'Login Error',
-                                text: 'Incorrect Username or password'
-                              }
-                        ).then((result) => {
-                            $('#button').removeAttr('disabled');
-                            });
-                    }
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
-        });
-    
+function loginSuccess() {
+  toastr.success("Redirecting you to the dashboard")
+  window.location.href = "/"
+}
+
+$("#login").submit(function () {
+  $("#button").prop("disabled", true)
+  $("#img").show()
+  event.preventDefault()
+  $.ajax({
+    url: "/checkloginpassword",
+    data: $(this).serialize(),
+    type: "POST",
+    success: function (response) {
+      $("#img").hide()
+      if (response === "correct") {
+        setTimeout(loginSuccess, 5000)
+        swal
+          .fire({
+            icon: "success",
+            title: "Login Successful",
+          })
+          .then((result) => {
+            window.location.href = "/"
+          })
+      } else if (response === "wrong") {
+        swal
+          .fire({
+            icon: "error",
+            title: "Login Error",
+            text: "Incorrect Username or password",
+          })
+          .then((result) => {
+            $("#button").removeAttr("disabled")
+          })
+      }
+    },
+    error: function (error) {
+      console.log(error)
+    },
+  })
+})
