8a313e3cea9733246f7c6e46cc8b7feaf3ec8343
Yoruba Developer <bukunmiadewale3@gmail.com>
2020-08-01 16:11:58 +0100

    Update __init__.py

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -8,14 +8,14 @@ app.secret_key = "ITSASECRET"
 app.config['MAIL_SERVER']='' #mail server
 app.config['MAIL_PORT'] = 587 #mail port
 app.config['MAIL_USERNAME'] = '' #email
-app.config['MAIL_PASSWORD'] = '' #password
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD'] #password
 app.config['MAIL_USE_TLS'] = True #security type
 app.config['MAIL_USE_SSL'] = False #security type
 
 #database connection parameters
 connection_params = {
     'user': '',
-    'password': '',
+    'password': os.environ['DB_PASSWORD'],
     'host': '',
     'port': 'port',
     'namespace': '',
